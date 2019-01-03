adefe8edc3fb6eb1f0b97973bacdc7e060df0dd6
Kostia <kbaidush@sphereinc.com>
2019-01-03 21:51:27 +0200

    [Feature] MEVN-3: JWT_AUTH Frontend features

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\User;
+use App\Http\Requests\RegisterFormRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterFormRequest $request)
+    {
+//        $user = new User;
+//        $user->email = $request->email;
+//        $user->name = $request->name;
+//        $user->password = bcrypt($request->password);
+//        $user->save();
+//
+//        return response([
+//            'status' => 'success',
+//            'data' => $user
+//        ], 200);
+
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return response()->json(['user' => $user]);
+    }
+
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+//        $credentials = $request->only('email', 'password');
+//
+//        if ( ! $token = JWTAuth::attempt($credentials)) {
+//            return response([
+//                'status' => 'error',
+//                'error' => 'invalid.credentials',
+//                'msg' => 'Invalid Credentials.'
+//            ], 400);
+//        }
+//
+//        return response([
+//            'status' => 'success'
+//        ])
+//            ->header('Authorization', $token);
+
+        $credentials = request(['email', 'password']);
+        if (! $token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+
+    public function user(Request $request)
+    {
+        $user = User::find(Auth::user()->id);
+
+        return response([
+            'status' => 'success',
+            'data' => $user
+        ]);
+    }
+
+//    public function refresh()
+//    {
+//        return response([
+//            'status' => 'success'
+//        ]);
+//    }
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth('api')->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'user' => $this->guard()->user(),
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function guard(){
+        return Auth::guard('api');
+    }
+//    public function logout()
+//    {
+//        JWTAuth::invalidate();
+//
+//        return response([
+//            'status' => 'success',
+//            'msg' => 'Logged out Successfully.'
+//        ], 200);
+//    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth('api')->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+}
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth('api')->user());
+    }
+
+}
